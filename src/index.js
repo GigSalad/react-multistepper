bc131c1c7adee1a8410c6b40ccfea0c88fddf27b
kory <kory.urban@gmail.com>
2018-01-09 11:19:43 -0500

    Try aliasing some imports to test something.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getNextStep } from './selector';
 import { addSteps } from './actions/steps';
-import { steps } from './reducers/steps';
-import { stepCounter } from './reducers/stepCounter';
+import { steps as stepsReducer } from './reducers/steps';
+import { stepCounter as stepCounterReducer } from './reducers/stepCounter';
 
 let mapStateToProps = function mapStateToProps(state) {
 	return {
